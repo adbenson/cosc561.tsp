3a69129eef8e23b5befc172a80c79bc2d7e2a80b
Drew Benson <drew@adbenson.net>
2014-11-06 23:07:20 -0500

    Well, that wasn't necessary

--- src/cosc561/tsp/util/CustomQueue.java
+++ src/cosc561/tsp/util/CustomQueue.java
@@ -153,26 +153,13 @@ public class CustomQueue<E> {
     /**
      * Inserts the specified element into this priority queue.
      *
-     * @return {@code true} (as specified by {@link Collection#add})
-     * @throws ClassCastException if the specified element cannot be
-     *         compared with elements currently in this priority queue
-     *         according to the priority queue's ordering
-     * @throws NullPointerException if the specified element is null
-     */
-    public boolean add(E e) {
-        return offer(e);
-    }
-
-    /**
-     * Inserts the specified element into this priority queue.
-     *
      * @return {@code true} (as specified by {@link Queue#offer})
      * @throws ClassCastException if the specified element cannot be
      *         compared with elements currently in this priority queue
      *         according to the priority queue's ordering
      * @throws NullPointerException if the specified element is null
      */
-    public boolean offer(E e) {
+    public boolean add(E e) {
         if (e == null)
             throw new NullPointerException();
         int i = size;
