8c827203478349c290e5c49914803e55771e1945
Drew Benson <drew@adbenson.net>
2014-11-22 11:37:06 -0500

    Starting worst-first insertion

---
+++ src/cosc561/tsp/strategy/tour_generation/WorstFirstInsertion.java
@@ -0,0 +1,73 @@
+package cosc561.tsp.strategy.tour_generation;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import cosc561.tsp.model.Graph;
+import cosc561.tsp.model.Node;
+import cosc561.tsp.model.branch.RichBranch;
+import cosc561.tsp.view.MapWindow;
+
+public class WorstFirstInsertion extends TourGenerator {
+	
+	private RichBranch tour;
+	
+	private Iterator<Node> unvisited;
+	private Node insert;
+	
+	private Iterator<Node> path;
+	private Node after;
+	
+	public WorstFirstInsertion(Graph graph, MapWindow window) {
+		super(graph, window);
+	}
+	
+
+	@Override
+	public void init() {
+		tour = new RichBranch(graph.getRoot(), graph);
+		
+		unvisited = tour.getUnvisited().iterator();
+		insert = unvisited.next();
+		
+		path = tour.getPath().iterator();
+		after = path.next();
+	}
+
+	@Override
+	protected RichBranch next() throws Exception {
+
+		LinkedList<Node> testTour = new LinkedList<>(tour.getPath());
+		
+		testTour.add(testTour.indexOf(after) + 1, insert);
+		
+		RichBranch testBranch = new RichBranch(testTour, graph);
+		
+		RichBranch worst = null;
+		if (worst == null || testBranch.weight > worst.weight){
+			worst = testBranch;
+		}
+		
+		tour = worst;
+		
+		if (! path.hasNext()) {
+			insert = unvisited.next();
+			path = tour.getPath().iterator();
+		}
+		
+		after = path.next();
+		
+		return testBranch;
+	}
+
+	@Override
+	public boolean isComplete() {
+		return (!unvisited.hasNext()) && (!path.hasNext());
+	}
+
+	@Override
+	public RichBranch getSolution() {
+		return tour;
+	}
+
+}
